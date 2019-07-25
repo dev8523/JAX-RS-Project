e284b54005e38c6fcefdc8d11f6007931e84fa07
deba8523 <36338136+deba8523@users.noreply.github.com>
2019-07-25 16:41:24 +0530

    Update DatabaseClass.java

--- messenger/src/main/java/org/debasish/infinite/messenger/database/DatabaseClass.java
+++ messenger/src/main/java/org/debasish/infinite/messenger/database/DatabaseClass.java
@@ -9,14 +9,9 @@ import org.debasish.infinite.messenger.model.Profile;
 public class DatabaseClass {
 
 	private static Map<Long, Message> messages = new HashMap<>();
-	private static Map<String, Profile> profiles = new HashMap<>();
 	
 	public static Map<Long, Message> getMessages() {
 		return messages;
-	}
-	
-	public static Map<String, Profile> getProfiles() {
-		return profiles;
 	}
 	
 }
